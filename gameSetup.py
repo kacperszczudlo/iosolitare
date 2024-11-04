0c5b4b765203b188d2541572d7a1be1e514327db
Fireeq <kamilkawa200@gmail.com>
2024-11-04 22:05:08 +0100

    update

--- gameSetup.py
+++ gameSetup.py
@@ -1,14 +1,12 @@
 from tkinter import Label, Button
 from PIL import Image, ImageTk
 from cardDeck import CardDeck
-from firstDeal import FirstDeal
 
 class GameSetup:
     def __init__(self, window):
         self.window = window
         self.card_labels = []
         self.deck = CardDeck()
-        self.first_deal = None
 
     def create_button(self, text, x, y, width, command=None):
         button = Button(self.window, text=text, font=("Arial", 12, "bold"), fg="white", bd=0, highlightthickness=0, bg="#5C4033", state="normal", command=command)
@@ -33,37 +31,6 @@ class GameSetup:
         card_label.image = card_photo
         card_label.place(x=x, y=y)
         return card_label
-
-    def display_initial_deal(self, columns):
-        y_offset = 378
-        y_spacing = 30
-
-        for col in range(7):
-            x_position = 131 + col * 140
-            for row, card in enumerate(columns[col]):
-                y_position = y_offset + row * y_spacing
-                card_label = self.create_card(x_position, y_position, card)
-                self.card_labels.append(card_label)
-
-    def display_stock_pile(self):
-        stock_pile_x, stock_pile_y = 131, 153
-        for i in range(len(self.first_deal.columns) * (len(self.first_deal.columns) + 1) // 2, len(self.deck.cards)):
-            self.deck.cards[i].hide()
-            card_label = self.create_card(stock_pile_x, stock_pile_y, self.deck.cards[i])
-            self.card_labels.append(card_label)
-
-    def reset_game(self):
-        for label in self.card_labels:
-            label.place_forget()
-        self.card_labels.clear()
-
-        self.deck = CardDeck()
-        self.deck.shuffle_deck()
-
-        self.first_deal = FirstDeal(self.deck)
-        columns = self.first_deal.setup_initial_layout()
-        self.display_initial_deal(columns)
-        self.display_stock_pile()
 
 
 

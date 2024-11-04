b03fa623805002428ba1291c997bf4e93f7f060f
KamilJop <kamiljop434@gmail.com>
2024-11-04 21:32:45 +0100

    Basic Window

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+width = 1200
+height = 800
+window = tk.Tk()
+window.title("Pasjans Inzynieria Oprogramowania")
+window.geometry(f"{width}x{height}")
+window.resizable(False, False)
+background = Image.open("resources/background.jpg")
+background = background.resize((width,height))
+background = ImageTk.PhotoImage(background)
 
-if __name__ == '__main__':
-    print('PyCharm')
+background_label = tk.Label(window, image=background)
+background_label.pack()
+
+
+window.mainloop()

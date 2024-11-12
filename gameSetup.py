1dca35265db5d350dd44b6aa79c6461f8da8c3c9
KamilJop <kamiljop434@gmail.com>
2024-11-12 21:50:56 +0100

    Podstawowy ruch

--- gameSetup.py
+++ gameSetup.py
@@ -31,6 +31,12 @@ class GameSetup:
         card_label = Label(self.window, image=card_photo, bd=0)
         card_label.image = card_photo
         card_label.place(x=x, y=y)
+        card_label.card_object = card
+
+        card_label.bind("<ButtonPress-1>", self.on_card_click)
+        card_label.bind("<B1-Motion>", self.on_card_drag)
+        card_label.bind("<ButtonRelease-1>", self.on_card_release)
+
         return card_label
 
     def display_initial_deal(self, columns):
@@ -64,5 +70,45 @@ class GameSetup:
         self.display_initial_deal(columns)
         self.display_stock_pile()
 
+    def on_card_click(self, event):
+
+        self.selected_card = event.widget.card_object
+        self.start_x = event.widget.winfo_x()
+        self.start_y = event.widget.winfo_y()
+        self.original_x = event.widget.winfo_x()
+        self.original_y = event.widget.winfo_y()
+
+        # To jest uzywane do odkladania karty
+        self.start_offset_x = event.x
+        self.start_offset_y = event.y
+
+        if not self.selected_card.revealed:
+            self.selected_card = None
 
 
+    def on_card_drag(self, event):
+        if self.selected_card:
+            new_x = event.widget.winfo_x() + (event.x - self.start_offset_x)
+            new_y = event.widget.winfo_y() + (event.y - self.start_offset_y)
+
+            event.widget.place(x=new_x, y=new_y)
+
+            self.start_x = new_x
+            self.start_y = new_y
+
+    def on_card_release(self, event):
+        if self.selected_card:
+            if self.is_valid_move():
+                self.place_in_column(event.x, event.y)
+            else:
+                event.widget.place(x=self.original_x, y=self.original_y)
+
+            self.selected_card = None
+
+    def is_valid_move(self):
+        # TODO: W następnym sprincie zaimplementuj logikę sprawdzania poprawności ruchu
+        return True
+
+    def place_in_column(self, x, y):
+        # TODO: W następnym sprincie zaimplementuj logikę umieszczania w kolumnie
+        pass
